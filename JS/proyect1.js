95b7fe48ca17199192b268a7dd2f2abcf30354a1
gonperez <gonperez18@gmail.com>
2021-07-26 19:44:35 -0300

    java2

--- JS/proyect1.js
+++ JS/proyect1.js
@@ -12,9 +12,27 @@ var mensajeEnviar= document.getElementById("cont-msj").value;
 nuevoPedido = new Pedido(nombreEnviar, mailEnviar,mensajeEnviar);
 console.log(nuevoPedido);
 agregar();
+saludo();
 }
 var baseDatos=[];
 function agregar (){
     baseDatos.push(nuevoPedido);
     console.log(baseDatos);
+    console.log("cantidad de consultas: " + baseDatos.length);
+};
+function saludo(){
+    var nombreSaludo = document.getElementById("formNombre").value;
+    var mailSaludo= document.getElementById("formMail").value;
+    alert(nombreSaludo + " se envio su consulta correctamente, te responderemos a la brevedad a " + mailSaludo);
+    sendOK()
+
 }
+function sendOK(){
+    var elemento = document.createElement("p"),
+        contenido = document.createTextNode("Mensaje enviado");
+    elemento.appendChild(contenido);
+    var sendOK = document.getElementById("prueba1");
+    sendOK.appendChild(elemento);
+    elemento.setAttribute("class", "prueba2")
+}
+
